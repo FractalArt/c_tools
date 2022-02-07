4f782e3ce496e05bbf7f3dd2abdc6398aa4afec5
fractalart <FractalArt@users.noreply.github.com>
2022-02-07 18:27:11 +0100

    Clean vec header file.

--- include/c_tools/vec.h
+++ include/c_tools/vec.h
@@ -12,29 +12,29 @@ typedef struct ct_vec_t {
 
 /** Push an element onto the vector.
  */
-void  ct_vec_push(ct_vec* const self, double x);
+void  ct_vec_push(ct_vec* const, double);
 
 /** Create a vector with capacity.
  */
-ct_vec ct_vec_with_capacity(size_t capacity);
+ct_vec ct_vec_with_capacity(size_t);
 
 /** Destruct the vector.
  *
  * Since the vector contains dynamically allocated
  * memory, this needs to be de-allocated after usage.
  */
-void ct_vec_destruct(ct_vec * const self);
+void ct_vec_destruct(ct_vec * const);
 
 /** Pop the last element from the vector.
  */
-double ct_vec_pop(ct_vec * const self);
+double ct_vec_pop(ct_vec * const);
 
 /** Get the capacity of the vector
  */ 
-inline size_t ct_vec_get_capacity(ct_vec * const self);
+size_t ct_vec_get_capacity(ct_vec * const);
 
 /** Get the size of the Vector
  */ 
-size_t ct_vec_get_size(ct_vec * const self);
+size_t ct_vec_get_size(ct_vec * const);
 
 #endif
